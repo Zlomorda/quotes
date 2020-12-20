587ca4ebf9b229a3ed8a81731a4a5a3ddde56df1
Zlomorda <daticho@gmail.com>
2020-12-20 23:19:57 +0200

    1st working version. Without style

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const allQuotes = require('./static/quotes.json')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
+app.set('static', path.join(__dirname, '/static'))
 
 app.get('/', (req, res) => {
-    res.render('home.ejs')
+    const randNum = Math.floor(Math.random() * (1643 - 1) + 1)
+    try {
+        const quoteText = allQuotes[randNum].text
+        const quoteAuthor = allQuotes[randNum].author
+        res.render('home.ejs', { quoteText, quoteAuthor })
+    } catch {
+        const quoteText = 'Something went wrong. Please refresh the page.'
+        const quoteAuthor = 'Developer'
+        res.render('home.ejs', { quoteText, quoteAuthor })
+    }
 })
 
 app.listen(8080, () => {
