8662388db63a7963bb1a6841fab923596bf3061b
Zlomorda <daticho@gmail.com>
2020-12-22 16:04:56 +0200

    robots.txtupdate

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const robots = require('express-robots-txt')
 const app = express()
 const path = require('path')
 const allQuotes = require('./static/quotes.json')
 
 app.use(express.static('static'))
+app.use(robots(__dirname + '/robots.txt'))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
