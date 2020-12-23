403e3691f26f2ad315db83409e9801fbbd1b31f8
Zlomorda <daticho@gmail.com>
2020-12-23 08:53:39 +0200

    Sitemap removed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const robots = require('express-robots-txt')
-const siteMap = require('express-sitemap-xml')
 const path = require('path')
 const allQuotes = require('./static/quotes.json')
 
@@ -8,11 +7,6 @@ const app = express()
 
 app.use(express.static('static'))
 app.use(robots(__dirname + '/robots.txt'))
-app.use(siteMap(getUrls, 'https://quotes-my.herokuapp.com/'))
-
-async function getUrls() {
-    return await getUrlsFromDatabase()
-}
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
